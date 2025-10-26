99e1f2800749e778f24a0acd50e697f6342640aa
JaretEduardo <kaiilawlietmx@gmail.com>
2025-10-26 01:28:16 -0600

    feat/consumption

--- src/app/features/tabs/consumption/consumption.component.ts
+++ src/app/features/tabs/consumption/consumption.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { IonAvatar, IonHeader, IonIcon, IonItem, IonLabel, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonList, IonListHeader } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { airplaneOutline, analyticsOutline, peopleOutline, qrCodeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-consumption',
   templateUrl: './consumption.component.html',
   styleUrls: ['./consumption.component.scss'],
+  imports: [
+    IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonAvatar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonList, IonItem, IonLabel, IonListHeader
+  ],
+  standalone: true
 })
 export class ConsumptionComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ qrCodeOutline, airplaneOutline, peopleOutline, analyticsOutline });
+  }
 
   ngOnInit() {}
 
