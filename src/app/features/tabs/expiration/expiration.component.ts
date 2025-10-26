8fda371f970678d945d69514680110411aaa6e5d
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-10-26 01:48:22 -0600

    feat(ui): vista expiracion y avance vista productividad

--- src/app/features/tabs/expiration/expiration.component.ts
+++ src/app/features/tabs/expiration/expiration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonAvatar, IonHeader, IonIcon, IonItem, IonLabel, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonList, IonListHeader } from '@ionic/angular/standalone';
+import { IonAvatar, IonHeader, IonIcon, IonItem, IonLabel, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonList, IonListHeader, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { alertCircleOutline, cameraOutline, settingsOutline, warningOutline } from 'ionicons/icons';
 
@@ -7,7 +7,7 @@ import { alertCircleOutline, cameraOutline, settingsOutline, warningOutline } fr
   selector: 'app-expiration',
   templateUrl: './expiration.component.html',
   styleUrls: ['./expiration.component.scss'],
-  imports: [IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonAvatar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonList, IonItem, IonLabel, IonListHeader],
+  imports: [IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonAvatar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonList, IonItem, IonLabel, IonListHeader, IonGrid, IonRow, IonCol],
   standalone: true,
 })
 export class ExpirationComponent  implements OnInit {
--- src/app/features/tabs/productivity/productivity.component.ts
+++ src/app/features/tabs/productivity/productivity.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonAvatar, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { checkmarkCircleOutline, timeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-productivity',
   templateUrl: './productivity.component.html',
   styleUrls: ['./productivity.component.scss'],
+  imports: [CommonModule, RouterModule, IonHeader, IonToolbar, IonItem, IonLabel, IonAvatar, IonContent, IonCard, IonCardContent, IonIcon, IonButton, IonList, IonListHeader, IonCardHeader, IonCardTitle],
+  standalone: true,
 })
 export class ProductivityComponent  implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ checkmarkCircleOutline, timeOutline });
+  }
 
   ngOnInit() {}
 
